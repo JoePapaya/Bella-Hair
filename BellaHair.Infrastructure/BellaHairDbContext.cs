1664d9a65326620399cceab6f497e404dde34ae9
JoePapaya <johannestvedegaard@outlook.dk>
2025-12-03 00:23:37 +0100

    Force add DbContext

---
+++ BellaHair.Infrastructure/BellaHairDbContext.cs
@@ -0,0 +1,22 @@
+﻿using BellaHair.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+
+namespace BellaHair.Infrastructure;
+
+public class BellaHairDbContext : DbContext
+{
+    public BellaHairDbContext(DbContextOptions<BellaHairDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<Kunde> Kunder => Set<Kunde>();
+    public DbSet<Medarbejder> Medarbejdere => Set<Medarbejder>();
+    public DbSet<Behandling> Behandlinger => Set<Behandling>();
+    public DbSet<Booking> Bookinger => Set<Booking>();
+    public DbSet<Rabat> Rabatter => Set<Rabat>();
+    public DbSet<Faktura> Fakturaer => Set<Faktura>();  
+
+}
+
